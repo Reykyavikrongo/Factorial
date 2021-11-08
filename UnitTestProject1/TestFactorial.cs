af0c9cbe05c48d9352ca702e79098e0707e5dbe7
Ante Žunić <anzunic@vsite.hr>
2021-11-08 19:05:04 +0100

    implement factorial function

--- UnitTestProject1/TestFactorial.cs
+++ UnitTestProject1/TestFactorial.cs
@@ -16,21 +16,21 @@ namespace Factorial.UnitTests
         [TestMethod]
         public void FactorialReturns1ForArgument1()
         {
-            var result = Math.Factorial(0);
+            var result = Math.Factorial(1);
             Assert.AreEqual(1, result);
         }
 
         [TestMethod]
         public void FactorialReturns2ForArgument2()
         {
-            var result = Math.Factorial(0);
+            var result = Math.Factorial(2);
             Assert.AreEqual(2, result);
         }
 
         [TestMethod]
         public void FactorialReturns24ForArgument4()
         {
-            var result = Math.Factorial(0);
+            var result = Math.Factorial(4);
             Assert.AreEqual(24, result);
         }
     }
